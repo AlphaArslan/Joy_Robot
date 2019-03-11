f55c48d02dd062ebdd94dcddc584a7ec5163baf7
AlphaArslan <arslan.alpha21@gmail.com>
2019-03-11 05:44:01 +0200

    count_faces() fix

--- RPi_One_Face_Detection/main.py
+++ RPi_One_Face_Detection/main.py
@@ -7,13 +7,12 @@ from picamera import PiCamera
 
 ################ Setup
 camera = PiCamera()
-rawCapture = PiRGBArray(camera)
 time.sleep(0.2)
-
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 ################ Functions
 def get_frame():
+    rawCapture = PiRGBArray(camera)
     camera.capture(rawCapture, format="bgr")
     frame = rawCapture.array
     return frame
@@ -21,7 +20,8 @@ def get_frame():
 def count_faces(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray)
-    return str(faces.shape[0])
+
+    return str(len(faces))
 
 if __name__ == '__main__':
     while True:
