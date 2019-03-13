d1b67305f5b6d4bc959e7b223c859d968917440b
AlphaArslan <arslan.alpha21@gmail.com>
2019-03-13 17:51:28 +0200

    added i2c commands

--- RPi_One_Face_Detection/main.py
+++ RPi_One_Face_Detection/main.py
@@ -3,12 +3,18 @@ import cv2
 import time
 from picamera.array import PiRGBArray
 from picamera import PiCamera
+from smbus2 import SMBus
 
 
 ################ Setup
 camera = PiCamera()
 time.sleep(0.2)
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+bus = SMBus(1)
+address = 0x04
+ONE_FACE_CODE = 1
+TWO_FACE_CODE = 2
 
 ################ Functions
 def get_frame():
@@ -20,12 +26,19 @@ def get_frame():
 def count_faces(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray)
+    return len(faces)
 
-    return str(len(faces))
+def send_command(faces = 0):
+    if faces is not 0 :
+        if faces is 1 :
+            bus.write_byte(address, ONE_FACE_CODE)
+        elif faces is 2:
+            bus.write_byte(address, TWO_FACE_CODE)
 
 if __name__ == '__main__':
     while True:
         frame = get_frame()
         faces_number = count_faces(frame)
+        send_command(faces = faces_number)
         print(faces_number)
         time.sleep(1)
