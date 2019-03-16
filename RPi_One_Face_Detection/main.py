dc28d69855a18b522fea6f44c5f9b77da24deff7
AlphaArslan <arslan.alpha21@gmail.com>
2019-03-16 20:18:31 +0200

    controliing servos over I2C

--- RPi_One_Face_Detection/main.py
+++ RPi_One_Face_Detection/main.py
@@ -4,6 +4,26 @@ import time
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 from smbus2 import SMBus
+from adafruit_servokit import ServoKit
+
+################ constants
+SALUT_DELAY       = 2                              # in seconds
+# I2C address
+ARDUINO_ADDRESS   = 0x04
+
+# servo angles
+SALUT_RU_ANGLE    = 90                             # Right upper servo angle for SALUT move
+SALUT_RL_ANGLE    = 45                             # Right Lower servo angle for SALUT move
+SALUT_LU_ANGLE    = 90                             # Left upper servo angle for SALUT move
+SALUT_LL_ANGLE    = 45                             # Left Lower servo angle for SALUT move
+
+# servo channel number
+RIGHT_UPPER_SERVO = 1
+RIGHT_LOWER_SERVO = 2
+LEFT_UPPER_SERVO  = 3
+LEFT_LOWER_SERVO  = 4
+HEAD_HOR_SERVO    = 5                              # horizontal
+HEAD_VER_SERVO    = 6                              # vertical
 
 
 ################ Setup
@@ -12,9 +32,11 @@ time.sleep(0.2)
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 bus = SMBus(1)
-address = 0x04
 ONE_FACE_CODE = 1
 TWO_FACE_CODE = 2
+
+kit = ServoKit(channels=16)
+
 
 ################ Functions
 def get_frame():
@@ -31,14 +53,36 @@ def count_faces(frame):
 def send_command(faces = 0):
     if faces is not 0 :
         if faces is 1 :
-            bus.write_byte(address, ONE_FACE_CODE)
+            bus.write_byte(ARDUINO_ADDRESS, ONE_FACE_CODE)
         elif faces is 2:
-            bus.write_byte(address, TWO_FACE_CODE)
+            bus.write_byte(ARDUINO_ADDRESS, TWO_FACE_CODE)
 
+def move_servos(faces):
+    if faces is 1:
+        kit.servo[RIGHT_UPPER_SERVO].angle = SALUT_RU_ANGLE
+        kit.servo[RIGHT_LOWER_SERVO].angle = SALUT_LU_ANGLE
+        time.sleep(SALUT_DELAY)
+        kit.servo[RIGHT_UPPER_SERVO].angle = 0
+        kit.servo[RIGHT_LOWER_SERVO].angle = 0
+
+    elif faces > 1:
+        kit.servo[RIGHT_UPPER_SERVO].angle = SALUT_RU_ANGLE
+        kit.servo[LEFT_UPPER_SERVO].angle  = SALUT_LU_ANGLE
+        kit.servo[RIGHT_LOWER_SERVO].angle = SALUT_RL_ANGLE
+        kit.servo[LEFT_LOWER_SERVO].angle  = SALUT_LL_ANGLE
+        time.sleep(SALUT_DELAY)
+        kit.servo[RIGHT_UPPER_SERVO].angle = 0
+        kit.servo[LEFT_UPPER_SERVO].angle  = 0
+        kit.servo[RIGHT_LOWER_SERVO].angle = 0
+        kit.servo[LEFT_LOWER_SERVO].angle  = 0
+
+
+################ Main
 if __name__ == '__main__':
     while True:
         frame = get_frame()
         faces_number = count_faces(frame)
         send_command(faces = faces_number)
+        move_servos(faces_number)
         print(faces_number)
         time.sleep(1)
